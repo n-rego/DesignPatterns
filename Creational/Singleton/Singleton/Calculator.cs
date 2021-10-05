9ea68384c3b495f1f2ae9faf27469b3fe3824719
Nuno Rego <nuno.rego@outlook.pt>
2021-10-05 21:27:26 +0100

    Adds Singleton pattern

---
+++ Creational/Singleton/Singleton/Calculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Creational.Singleton
+{
+    public sealed class Calculator
+    {
+
+        private static Calculator _instance;
+        private readonly List<int> _memory = new();
+
+        private Calculator()
+        {
+
+        }
+
+        public static Calculator GetInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new Calculator();
+            }
+
+            return _instance;
+        }
+
+        public void AddNumberToMemory(int number)
+        {
+            _memory.Add(number);
+        }
+
+        public void ClearNumbersFromMemory()
+        {
+            _memory.Clear();
+        }
+
+        public int GetSumOfNumbersInMemory()
+        {
+            int sum = 0;
+            _memory.ForEach((number) => sum += number);
+
+            return sum;
+        }
+    }
+}
---
+++ Creational/Singleton/Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Patterns.Creational.Singleton
+{
+    internal static class Program
+    {
+        internal static void Main()
+        {
+            const int number = 10;
+            for(int i = 0; i < number; i++)
+            {
+                Calculator.GetInstance().AddNumberToMemory(1);
+            }
+
+            Console.WriteLine($"Calculator summed numbers in memory up to: {Calculator.GetInstance().GetSumOfNumbersInMemory()}");
+            Calculator.GetInstance().ClearNumbersFromMemory();
+        }
+    }
+}
